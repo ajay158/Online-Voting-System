8a50550407322ab727bf2c7a5769eca55cf2d675
Ajay Kumar Shahu <1905922@kiit.ac.in>
2024-07-17 08:26:25 +0545

    Online Voting System

---
+++ routes/dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Vpting System - Dashboard</title>
+</head>
+
+<body>
+    <button>Back</button>
+    <button>Logout</button>
+    <h1>Online Voing System</h1>
+    <hr>
+    <div id=""></div>
+    <div id=""></div>
+
+</body>
+
+</html>
