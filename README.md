7327253a59b1b0d67aad72df5107820767bb2978
Ajay Kumar Shahu <1905922@kiit.ac.in>
2024-07-17 17:32:29 +0545

    online voting system

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Online-Voting-System
+
 In this website, I have created a online voting system where people can vote online.
+
+<?php
+session_start();
+if (!isset($_SESSION['userdata'])) {
+    header("location: ../");
+}
+
+?>
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,13 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Vpting System - Dashboard</title>
+    <title>Online Voting System - Dashboard</title>
+    <link rel="stylesheet" href="../css/style.css" />
 </head>
 
 <body>
-    <button>Back</button>
-    <button>Logout</button>
-    <h1>Online Voing System</h1>
+
+    <style>
+        .backbtn {
+            float: left;
+            margin-top: 22px;
+            margin-left: 60px;
+        }
+
+        .logoutbtn {
+            float: right;
+            margin-top: 22px;
+            margin-right: 60px;
+        }
+    </style>
+
+    <button class="backbtn" id="loginbtn">Back</button>
+    <button class="logoutbtn" id="loginbtn">Logout</button>
+    <div id="headerSection">
+        <h1>Online Voting System</h1>
+    </div>
     <hr>
     <div id=""></div>
     <div id=""></div>
