26f2812c3d5c3bf9b2c62d6a14807024a9790fbd
Ajay Kumar Shahu <1905922@kiit.ac.in>
2024-07-17 21:40:55 +0545

    Online Voting System

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # Online-Voting-System
 
 In this website, I have created a online voting system where people can vote online.
-
-<?php
-session_start();
-if (!isset($_SESSION['userdata'])) {
-    header("location: ../");
-}
-
-?>
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -3,10 +3,16 @@ session_start();
 if (!isset($_SESSION['userdata'])) {
     header("location: ../");
 }
-
-?>
-
 $userdata = $_SESSION['userdata'];
+$groupsdata = $_SESSION['groupsdata'];
+
+if($_SESSION ['userdata'] = $_SESSION['status']==0){
+    $status = '<b style = "color:red" > Not Voted </b>'
+}
+else{
+    $status = '<b style = "color:green"> Voted </b>'
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -46,29 +52,64 @@ $userdata = $_SESSION['userdata'];
             padding: 15px;
             float: right;
         }
+
+        #mainpanel {
+            padding: 10px;
+        }
     </style>
 
     <div id="mainSection">
         <center>
             <div id="headerSection">
-                <button class="backbtn" id="loginbtn">Back</button>
-                <button class="logoutbtn" id="loginbtn">Logout</button>
+            <a href="../"><button class="backbtn" id="loginbtn"> Back</button></a> 
+            <a href="logout.php"> <button class="logoutbtn" id="loginbtn"> Logout</button></a> 
                 <h1>Online Voting System</h1>
             </div>
         </center>
         <hr>
-        <div id="profile">
-            <center> <img src="../uploads/<?php echo $userdata['photo'] ?>" height="100" width="100"> </center><br><br>
-            <b>Name: </b> <?php echo $userdata['name'] ?><br><br>
-            <b>Mobile: </b> <?php echo $userdata['mobile'] ?><br><br>
-            <b>Address: </b> <?php echo $userdata['address'] ?><br><br>
-            <b>Status: </b> <?php echo $userdata['status'] ?><br><br>
+        <div id="mainpanel">
 
-        </div>
-        <div id="Group">
-            if($_SESSION['groupdata'];){
+            <div id="profile">
+                <center> <img src="../uploads/<?php echo $userdata['photo'] ?>" height="100" width="100"> </center><br><br>
+                <b>Name: </b> <?php echo $userdata['name'] ?><br><br>
+                <b>Mobile: </b> <?php echo $userdata['mobile'] ?><br><br>
+                <b>Address: </b> <?php echo $userdata['address'] ?><br><br>
+                <b>Status: </b> <?php echo $status ?><br><br>
 
-            }
+            </div>
+            <div id="Group">
+                <?php
+                if ($_SESSION['groupsdata']) {
+                    for ($i = 0; $i < count($groupsdata); $i++) {
+                ?>
+                        <div>
+                            <img style="float: right;" src="../uploads/ <?php echo $groupsdata[$i]['photo'] ?>>" height="100" width="100">
+                            <b>Group Name: </b><?php echo $groupsdata[$i]['name'] ?> <br><br>
+                            <b>Votes: </b> <?php echo $groupsdata[$i]['votes'] ?> <br><br>
+                            <form action="../api/vote.php" method="POST">
+                                <input type="hidden" name="gvotes" value="<?php echo $groupsdata[$i]['votes'] ?>">
+                                <input type="hidden" name="gid" value="<?php echo $groupsdata[$i]['id'] ?>">
+                                    <?php 
+                                    if($_SESSION['userdata'] ['status']==0){
+
+                                        ?>
+                                        <input type="submit" name="votebtn" value="vote" id="votebtn" id="loginbtn">
+                                        <?php
+                                    }
+                                    else{
+                                        ?>
+                                        <button disabled type="button" name="votebtn" value="vote" id="loginbtn"></button>
+                                        <?php
+                                    }
+                            </form>
+                        </div>
+                        <hr>
+                <?php
+                    }
+                } else {
+                }
+                ?>
+            </div>
         </div>
     </div>
 
