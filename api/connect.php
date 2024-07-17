1e8f5fabc3dbc3d336da054d5699a319f03f533e
Ajay Kumar Shahu <1905922@kiit.ac.in>
2024-07-17 14:45:40 +0545

    Online Voting System

---
+++ api/connect.php
@@ -0,0 +1,7 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "onlinevotingsystem") or die("connection failed");
+if ($connect) {
+    echo "Connected!";
+} else {
+    echo "Connection failed!";
+}
